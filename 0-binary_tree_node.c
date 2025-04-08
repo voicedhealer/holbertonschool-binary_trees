ab033c2c4034acdd4384beed66474b800e92935b
voicedhealer <vivien.bernardot@gmail.com>
2025-04-08 16:13:51 +0200

    amélioration de la func binary tree node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -9,18 +9,18 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node;
+	binary_tree_t *new_node;
 
-    /* Allouer de la mémoire pour le nouveau nœud */
-    new_node = malloc(sizeof(binary_tree_t));
-    if (new_node == NULL)
-        return (NULL);
+	/* Allouer de la mémoire pour le nouveau nœud */
+	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
+		return (NULL);
 
-    /* Initialiser les champs du nœud */
-    new_node->n = value;       /* Assigner la valeur au champ 'n' */
-    new_node->parent = parent; /* Assigner le parent */
-    new_node->left = NULL;     /* Pas d'enfant gauche au départ */
-    new_node->right = NULL;    /* Pas d'enfant droit au départ */
+	/* Initialiser les champs du nœud */
+	new_node->n = value;		/* Assigner la valeur au champ 'n' */
+	new_node->parent = parent;	/* Assigner le parent */
+	new_node->left = NULL;		/* Pas d'enfant gauche au départ */
+	new_node->right = NULL;		/* Pas d'enfant droit au départ */
 
-    return (new_node);
+	return (new_node);
 }
