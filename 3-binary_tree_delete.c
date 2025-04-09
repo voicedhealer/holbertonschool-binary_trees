7ddcf6b2278040e4f0828fe33d69248bed4d3f28
voicedhealer <vivien.bernardot@gmail.com>
2025-04-09 16:08:55 +0200

    update : meilleur lecture des commentaires et standrad betty optimisé

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,19 +1,15 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_right - Insère un nœud comme enfant droit
- * @parent: Pointeur vers le nœud parent
- * @value: Valeur à stocker dans le nouveau nœud
- * Return: Pointeur vers le nouveau nœud ou NULL
+ * binary_tree_delete - Supprime récursivement un arbre binaire
+ * @tree: Pointeur vers le nœud racine de l'arbre à supprimer
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
-   if (!tree)
-   {
-    return;
-   }
-   binary_tree_delete(tree->left);
-   binary_tree_delete(tree->right);
+	if (!tree)
+		return;
 
-   free(tree);
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+	free(tree);
 }
