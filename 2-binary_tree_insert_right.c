3f0f53c48e21f7edec023cf6bd4564ce538212c2
voicedhealer <vivien.bernardot@gmail.com>
2025-04-09 16:14:05 +0200

    update: betty style

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,28 +1,29 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_right - Insère un nœud comme enfant droit
- * @parent: Pointeur vers le nœud parent
- * @value: Valeur à stocker dans le nouveau nœud
- * Return: Pointeur vers le nouveau nœud ou NULL
+ * binary_tree_insert_right - Inserts a node as the right-child of a given node
+ * @parent: Pointer to the node to insert the right-child in
+ * @value: Value to store in the new node
+ *
+ * Return: Pointer to the created node, or NULL on failure
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node;
+	binary_tree_t *new_node;
 
-    if (!parent)
-        return (NULL);
+	if (!parent)
+		return (NULL);
 
-    new_node = binary_tree_node(parent, value);
-    if (!new_node)
-        return (NULL);
+	new_node = binary_tree_node(parent, value);
+	if (!new_node)
+		return (NULL);
 
-    if (parent->right)
-    {
-        new_node->right = parent->right;
-        parent->right->parent = new_node;
-    }
-    parent->right = new_node;
+	if (parent->right)
+	{
+		new_node->right = parent->right;
+		parent->right->parent = new_node;
+	}
+	parent->right = new_node;
 
-    return (new_node);
+	return (new_node);
 }
